894d5c0f72bbd016a4144d6532dcb97d60e15c20
OmarWaled21 <omar.waled484@gmail.com>
2025-01-11 23:40:53 +0200

    add event reminder

--- lib/views/booking_view.dart
+++ lib/views/booking_view.dart
@@ -1,5 +1,6 @@
 import 'package:barber_shop/helper/media_query_extention.dart';
 import 'package:barber_shop/helper/navigator_extention.dart';
+import 'package:barber_shop/services/add_2_calender_service.dart';
 import 'package:barber_shop/services/time_day_service.dart';
 import 'package:barber_shop/views/day_selector_view.dart';
 import 'package:barber_shop/views/time_selector_view.dart';
@@ -20,9 +21,7 @@ class _BookingDateViewState extends State<BookingDateView> {
 
   void _updateSelectedDay(int day) async {
     setState(() => _selectedDay = day);
-    // Create time slots for the selected day
     await TimeDayService().createTimeSlotsForNewDay(day);
-    // Reset the selected time slot when the day changes
     setState(() => _selectedTimeSlot = null);
   }
 
@@ -50,9 +49,7 @@ class _BookingDateViewState extends State<BookingDateView> {
             child: TimeSlotSelectorView(
               selectedSlot: _selectedTimeSlot,
               onSlotSelected: _updateSelectedTimeSlot,
-              dayStream: TimeDayService().getTimeSlotsStream(
-                _selectedDay,
-              ),
+              dayStream: TimeDayService().getTimeSlotsStream(_selectedDay),
               selectedDate: _selectedDay,
             ),
           ),
@@ -62,11 +59,8 @@ class _BookingDateViewState extends State<BookingDateView> {
             child: CustomButton(
               onPressed: _selectedTimeSlot != null
                   ? () async {
-                      await TimeDayService().updateTimeSlotAvailability(
-                        _selectedDay,
-                        _selectedTimeSlot!,
-                        false,
-                      );
+                      await _changeAvailablity();
+                      _addToCalendar();
                     }
                   : null,
               title: 'Continue',
@@ -75,5 +69,32 @@ class _BookingDateViewState extends State<BookingDateView> {
         ],
       ),
     );
+  }
+
+  Future<void> _changeAvailablity() async {
+    await TimeDayService().updateTimeSlotAvailability(
+      _selectedDay,
+      _selectedTimeSlot!,
+      false,
+    );
+  }
+
+  void _addToCalendar() async {
+    if (_selectedTimeSlot != null) {
+      // Get the parsed start time
+      final startTime = AddToCalendarService()
+          .parseSelectedTimeSlot(_selectedDay, _selectedTimeSlot!);
+
+      // Set the event duration (e.g., 1 hour)
+      final endTime = startTime.add(const Duration(hours: 1));
+
+      // Add the event to the calendar
+      await AddToCalendarService().addEventToCalendar(
+        title: 'Booking Appointment',
+        startTime: startTime,
+        endTime: endTime,
+        description: 'Your booking at Barber Shop',
+      );
+    }
   }
 }
--- lib/widgets/time_slot_grid.dart
+++ lib/widgets/time_slot_grid.dart
@@ -7,7 +7,7 @@ class TimeSlotGrid extends StatelessWidget {
   final String? selectedSlot;
   final bool isToday;
   final int currentHour;
-  final int currentMinute; // Added currentMinute
+  final int currentMinute;
   final ValueChanged<String> onSlotSelected;
 
   const TimeSlotGrid({
@@ -16,7 +16,7 @@ class TimeSlotGrid extends StatelessWidget {
     required this.selectedSlot,
     required this.isToday,
     required this.currentHour,
-    required this.currentMinute, // Added currentMinute
+    required this.currentMinute,
     required this.onSlotSelected,
   });
 
@@ -42,16 +42,23 @@ class TimeSlotGrid extends StatelessWidget {
         final hour = int.parse(parts[0].split(':')[0]);
         final minute = int.parse(parts[0].split(':')[1]);
         final period = parts[1];
-        int adjustedHour = (period == "PM" && hour != 12)
-            ? hour + 12
-            : (period == "AM" && hour == 12)
-                ? 0
-                : hour;
 
-        // Compare hour and minute to determine if the slot is past
-        final isPast = isToday &&
-            (adjustedHour < currentHour ||
-                (adjustedHour == currentHour && minute < currentMinute));
+        // Convert the time slot to 24-hour format
+        int adjustedHour = hour;
+        if (period == "PM" && hour != 12) {
+          adjustedHour += 12; // Convert PM to 24-hour format
+        } else if (period == "AM" && hour == 12) {
+          adjustedHour = 0; // Convert 12:00 AM to 00:00
+        }
+
+        // Compare the time slot with the current time
+        bool isPast = false;
+        if (isToday) {
+          if (adjustedHour < currentHour ||
+              (adjustedHour == currentHour && minute < currentMinute)) {
+            isPast = true;
+          }
+        }
 
         return TimeSlotCard(
           timeSlot: time,
